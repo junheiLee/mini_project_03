e238c2db547b4d48f4b34969650389ef2698fac6
junheiLee <jun2ee22@naver.com>
2023-08-03 16:22:49 +0900

    refactor: 상품 카테고리별 목록, 상품 디테일 컨트롤러 생성

--- src/com/team08/service/ProductService.java
+++ src/com/team08/service/ProductService.java
@@ -1,9 +1,40 @@
 package com.team08.service;
 
+import java.util.ArrayList;
+
+import com.team08.dao.ProductDAO;
+import com.team08.dto.ProductVO;
+
 public class ProductService {
 
-	public ProductService() {
-		// TODO Auto-generated constructor stub
+	private static ProductService instance = new ProductService();
+	private ProductDAO dao;
+	
+	private ProductService() {
+		this.dao = ProductDAO.getInstance();
+	}
+	
+	public static ProductService getInstance() {
+		return instance;
+	}
+	
+	/**
+	 * 상품 번호에 해당하는 ProductVO 가져오는 메서드
+	 * 
+	 * @param pseq 상품 번호
+	 * @return ProductVO
+	 */
+	public ProductVO getProduct(String pseq) {
+		return dao.getProduct(pseq);
 	}
 
+	/**
+	 * 상품 목룍에 해당하는 상품 목록 가져오는 메서드	
+	 * 
+	 * @param kind 상품 종료
+	 * @return ArrayList<ProductVO>
+	 */
+	public ArrayList<ProductVO> listKindProduct(String kind) {
+		return dao.listKindProduct(kind);
+	}
 }
