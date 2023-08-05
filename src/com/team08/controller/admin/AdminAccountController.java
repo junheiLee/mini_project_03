6c03a21cdc0329203fa6970d7c5c821bed3ba154
junheiLee <jun2ee22@naver.com>
2023-08-05 16:15:00 +0900

    refactor: admin 뼈대2

--- src/com/team08/controller/admin/AdminAccountController.java
+++ src/com/team08/controller/admin/AdminAccountController.java
@@ -7,15 +7,32 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-@WebServlet("/admin/logins/*")
-public class AdminLoginController extends HttpServlet {
+/**
+ * Servlet implementation class AdminAccountController
+ */
+@WebServlet("/AdminAccountController")
+public class AdminAccountController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AdminAccountController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println(request.getPathInfo());
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
